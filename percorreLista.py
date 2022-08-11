cf825582da20abc71c6387f4806b42a959d62934
igorcamargo1 <rm94553@fiap.com.br>
2022-08-11 09:46:46 -0300

    criando um percorredor de lista

---
+++ percorreLista.py
@@ -0,0 +1,11 @@
+def percorreLista(lista):
+    print("Percorrendo uma lista...")
+    i = 0
+    while i < len(lista):
+        print(f"Elemento: {lista[i]}")
+        i+=1
+#=======================================
+# Programa Principal
+
+lista = [1,2,3,4,5]
+percorreLista(lista)
