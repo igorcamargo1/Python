f1d0e3088b955cc37fad0044c67c9c781bdd1f7a
igorcamargo1 <rm94553@fiap.com.br>
2022-08-11 08:32:21 -0300

    programa que usa funções para saber o dobro de um número

---
+++ calculaDobro.py
@@ -0,0 +1,27 @@
+'''1.Escreva  um  programa  que  leia  um  número  do  teclado,  calcule  o  dobro  do número
+ lido e imprima o resultado. O programa deve ter:
+ •Função para entrada de dados
+ •Função para calcular o dobro do número
+ •Função para imprimir o resultado
+ •Programa Principal para testar as funções acima'''
+
+print("Calcula dobro")
+print("==============")
+
+#===============================================
+def entradaDados():
+    n = int(input("Digite o número que você deseja dobrar:"))
+    return n
+#===============================================
+def calculaDobro(n):
+    dobro = n*2
+    return dobro
+#===============================================
+def imprimir(result):
+    print(f"o resultado foi {result}")
+#===============================================
+#programa principal
+
+entrada = entradaDados()
+processo = calculaDobro(entrada)
+saida = imprimir(processo)
