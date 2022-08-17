ecaffe1f7fcc39277f3154aa49c44718599f77bf
igorcamargo <rm94553@fiap.com.br>
2022-08-17 10:33:48 -0300

    exemplo de listas

---
+++ listas/exemplo1.py
@@ -0,0 +1,11 @@
+lista = [2,10,5,-3,20]
+
+def imprimeElementos():
+    i=0
+    while i < len(lista):
+        print(f'Elemento: {lista[i]}')
+        i+=1
+#--------------------------------------
+#Programa Principal
+
+imprimeElementos()
