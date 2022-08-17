9723f37868b12f4a91d54042c69d299d5517dfb2
igorcamargo <rm94553@fiap.com.br>
2022-08-17 11:19:35 -0300

    criar e manipular lista

--- listas/exemplo1.py
+++ listas/exemplo1.py
@@ -5,7 +5,12 @@ def imprimeElementos():
     while i < len(lista):
         print(f'Elemento: {lista[i]}')
         i+=1
+        lista[0] = 2000
 #--------------------------------------
 #Programa Principal
 
 imprimeElementos()
+print("-------------")
+imprimeElementos()
+
+#--------------------------------------
---
+++ listas/exemplo2.py
@@ -0,0 +1,38 @@
+#atividade - manipulação de listas com funções
+'''
+1) Criar função tamanho_lista()
+2) Criar função criar_lista()
+3) Criar função imprimir_lista()
+4) Criar função imprimir_pares()
+5) Criar função imprimir_impares()
+6) Criar função adicione os elementos impares em uma lista()
+7) Criar função adicione os elementos pares em uma lista()
+8) Crias função de busca de um elemento em uma lista()
+'''
+#--------------------------------------------------
+def tamanho_lista():
+    print('-- Tamanho da Lista --')
+    t = int(input("Qual é o tamanho da lista:  "))
+    return t
+#--------------------------------------------------
+def criar_lista(t):
+    lista = []
+    print('-- Criando lista... --')
+    print("----------------------")
+    i = 0
+    while i < t:
+        lista[i] = int(input('Número:'))
+        i += 1
+    return lista
+#--------------------------------------------------
+def imprimir_lista(lista):
+    print('-- Imprimindo os elementos da lista: --')
+    for i in lista:
+        print(f'Elemento: {i}')
+#--------------------------------------------------
+# Programa Principal
+t = tamanho_lista()
+lista = criar_lista(t)
+imprimir = imprimir_lista(lista)
+
+
